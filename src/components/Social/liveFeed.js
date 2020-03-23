34c51a3701f422b2771a9a3777a75dca0844c865
ash-sp-cole <ash_sp_cole@hotmail.co.uk>
2020-03-23 15:45:14 +0100

    update

--- src/components/Social/liveFeed.js
+++ src/components/Social/liveFeed.js
@@ -67,10 +67,7 @@ const LiveFeed = () => (
           <Feed.Date>3 days ago</Feed.Date>
         </Feed.Summary>
         <Feed.Extra text>
-          Ours is a life of constant reruns. We're always circling back to where
-          we'd we started, then starting all over again. Even if we don't run
-          extra laps that day, we surely will come back for more of the same
-          another day soon.
+          Ours is a life of constant reruns.
         </Feed.Extra>
         <Feed.Meta>
           <Feed.Like>
